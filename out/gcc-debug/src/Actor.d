03e034307a5b9ef75ca547f3a7c9b74bfa0e561f
Alexander Hoffmann <alexander.hoffmann3@haw-hamburg.de>
2017-10-26 17:20:23 +0200

    .gitignore eingefügt und result umgezogen

--- out/gcc-debug/src/Actor.d
+++
@@ -1,6 +0,0 @@
-../out/gcc-debug/src/Actor.o: Actor.cc Actor.h \
- /home/alexander/omnetpp-5.1.1/include/omnetpp.h
-
-Actor.h:
-
-/home/alexander/omnetpp-5.1.1/include/omnetpp.h:
--- out/gcc-debug/src/Spiderman.d
+++
@@ -1,8 +0,0 @@
-../out/gcc-debug/src/Spiderman.o: Spiderman.cc ../src/Spiderman.h \
- /home/alexander/omnetpp-5.1.1/include/omnetpp.h ../src/Actor.h
-
-../src/Spiderman.h:
-
-/home/alexander/omnetpp-5.1.1/include/omnetpp.h:
-
-../src/Actor.h:
